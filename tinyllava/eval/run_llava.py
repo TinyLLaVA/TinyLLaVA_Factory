45e655982b589ca3dbb0cc7418ee0e8ada7c850f
Your Name <you@example.com>
2024-02-23 22:37:28 +0800

    update evaluation script

--- tinyllava/eval/run_llava.py
+++ tinyllava/eval/run_llava.py
@@ -70,23 +70,26 @@ def eval_model(args):
         else:
             qs = DEFAULT_IMAGE_TOKEN + "\n" + qs
 
-    if "llama-2" in model_name.lower():
-        conv_mode = "llava_llama_2"
-    elif "v1" in model_name.lower():
-        conv_mode = "llava_v1"
-    elif "mpt" in model_name.lower():
-        conv_mode = "mpt"
-    else:
-        conv_mode = "llava_v0"
 
-    if args.conv_mode is not None and conv_mode != args.conv_mode:
-        print(
-            "[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}".format(
-                conv_mode, args.conv_mode, args.conv_mode
-            )
-        )
-    else:
-        args.conv_mode = conv_mode
+    # if 'phi' in model_name.lower() or '3.1b' in model_name.lower():
+    #     conv_mode = "phi"
+    # if "llama-2" in model_name.lower():
+    #     conv_mode = "llava_llama_2"
+    # elif "v1" in model_name.lower():
+    #     conv_mode = "llava_v1"
+    # elif "mpt" in model_name.lower():
+    #     conv_mode = "mpt"
+    # else:
+    #     conv_mode = "llava_v0"
+    #
+    # if args.conv_mode is not None and conv_mode != args.conv_mode:
+    #     print(
+    #         "[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}".format(
+    #             conv_mode, args.conv_mode, args.conv_mode
+    #         )
+    #     )
+    # else:
+    # args.conv_mode = conv_mode
 
     conv = conv_templates[args.conv_mode].copy()
     conv.append_message(conv.roles[0], qs)
@@ -124,14 +127,14 @@ def eval_model(args):
             stopping_criteria=[stopping_criteria],
         )
 
-    input_token_len = input_ids.shape[1]
-    n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
-    if n_diff_input_output > 0:
-        print(
-            f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids"
-        )
+    # input_token_len = input_ids.shape[1]
+    # n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+    # if n_diff_input_output > 0:
+    #     print(
+    #         f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids"
+    #     )
     outputs = tokenizer.batch_decode(
-        output_ids[:, input_token_len:], skip_special_tokens=True
+        output_ids, skip_special_tokens=True
     )[0]
     outputs = outputs.strip()
     if outputs.endswith(stop_str):
