6042ebfaea377cdcf1193c0fa89301db202f640e
Your Name <you@example.com>
2024-02-23 14:44:40 +0800

    modify scripts

--- scripts/tiny_llava/pretrain_baselines.sh
+++
@@ -1,9 +0,0 @@
-LLM_VERSION=TinyLlama/TinyLlama-1.1B-Chat-v1.0
-VT_VERSIONS=(openai/clip-vit-large-patch14-336 openai/clip-vit-large-patch14 openai/clip-vit-base-patch16 openai/clip-vit-large-patch32)
-DATA_PATH=/root/autodl-tmp/data/llava/blip_laion_cc_sbu_558k.json
-IMAGE_PATH=/root/autodl-tmp/data/llava/llava_pretrain/images
-
-
-for VT_VERSION in "${VT_VERSIONS[@]}"; do
-    bash scripts/tiny_llava/pretrain.sh "$LLM_VERSION" "$VT_VERSION" "$DATA_PATH" "$IMAGE_PATH"
-done
