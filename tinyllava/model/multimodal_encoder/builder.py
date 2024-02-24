df9db4f832ab6cc7135d82544f73e3f2e3186f62
jiajunlong <1092924403@qq.com>
2024-02-24 18:32:17 +0800

    delete z

--- tinyllava/model/multimodal_encoder/builder.py
+++ tinyllava/model/multimodal_encoder/builder.py
@@ -1,6 +1,6 @@
 import os
 from tinyllava.model.multimodal_encoder.clip_encoder import CLIPVisionTower
-from tinyllava.model.multimodal_encoder.siglip_encoder import SigLipVisionTowerz
+from tinyllava.model.multimodal_encoder.siglip_encoder import SigLipVisionTower
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
