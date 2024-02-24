7a77fe40247f21e717864b8a1f80f93e19768bc0
jiajunlong <1092924403@qq.com>
2024-02-24 18:24:35 +0800

    modify multimodal_encoder builder

--- tinyllava/model/multimodal_encoder/builder.py
+++ tinyllava/model/multimodal_encoder/builder.py
@@ -1,12 +1,14 @@
 import os
 from tinyllava.model.multimodal_encoder.clip_encoder import CLIPVisionTower
-from tinyllava.model.multimodal_encoder.siglip_encoder import SigLipVisionTower
+from tinyllava.model.multimodal_encoder.siglip_encoder import SigLipVisionTowerz
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
     vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
     is_absolute_path_exists = os.path.exists(vision_tower)
-    if (is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion")) and "siglip" not in vision_tower.lower():
-        return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-    elif is_absolute_path_exists or vision_tower.startswith("google") or vision_tower.startswith('bczhou') or 'siglip' in vision_tower.lower():
-        return SigLipVisionTower(vision_tower, vision_tower_cfg, **kwargs)
+    if 'siglip' not in vision_tower.lower():
+        if is_absolute_path_exists or vision_tower.startswith('openai') or vision_tower.startswith('laion'):
+            return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    else:
+        if is_absolute_path_exists or vision_tower.startswith("google") or vision_tower.startswith('bczhou'):
+            return SigLipVisionTower(vision_tower, vision_tower_cfg, **kwargs)
     raise ValueError(f'Unknown vision tower: {vision_tower}')
