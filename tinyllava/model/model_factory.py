f457ae7e8453f3e94fc7bd41e7a211c3b6374e5c
baichuanzhou <baichuanzhou@buaa.edu.cn>
2024-03-15 12:37:02 +0800

    fix model factory mixup

--- tinyllava/model/model_factory.py
+++ tinyllava/model/model_factory.py
@@ -20,13 +20,6 @@ def TokenizerSelect(model_name_or_path):
     for name in TOKENIZER_REGISTRY.keys():
         if name.lower() in model_name_or_path.lower():
             tokenizer_init = TOKENIZER_REGISTRY[name]
-        # FIXME: Find a more elegant way to do this
-        if name == 'phi':
-            MODEL_REGISTRY['TinyLLaVA-3.1B'] = cls
-        elif name == 'stablelm':
-            MODEL_REGISTRY['TinyLLaVA-2.0B'] = cls
-        elif name == 'llama':
-            MODEL_REGISTRY['TinyLLaVA-1.5B'] = cls
     if tokenizer_init is None:
         tokenizer_init = TOKENIZER_REGISTRY['llama']
     return tokenizer_init
