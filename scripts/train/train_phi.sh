04139fd19777ed71e75bf99a6a9db579d83b80bf
Xingjian Zhang <99701420+ZhangXJ199@users.noreply.github.com>
2025-04-25 14:20:52 +0800

    Update train_phi.sh

--- scripts/train/train_phi.sh
+++ scripts/train/train_phi.sh
@@ -10,7 +10,7 @@ CN_VERSION=mlp2x_gelu #connector type, other options are: qformer, resampler, et
 CONV_VERSION=phi #chat template, other options are: phi, llama, gemmma, etc
 VERSION=base #experiment name for recording different runnings
 TRAIN_RECIPE=common #training recipes, other options are: lora, qlora
-MODEL_MAX_LENGTH=3072 #max model length for llm
+MODEL_MAX_LENGTH=2048 #max model length for llm
 
 
 bash scripts/train/pretrain.sh "$DATA_PATH" "$IMAGE_PATH" "$LLM_VERSION" "$VT_VERSION" "$VT_VERSION2" "$CN_VERSION" "$VERSION" "$TRAIN_RECIPE" "$MODEL_MAX_LENGTH"
